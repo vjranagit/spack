vjranagit/spack

commit e06c974424f41425ed5b6fa6a08e1feb7411fa91

SPACK-38: Allow specs to be indexed by virtual dependencies.
- The following now work differently:

      spec['mpi']
      spec['blas']

  This can return a spec for openmpi, mpich, mvapich, etc., EVEN if
  the spec is already concretized.  This means that in a package that
  `depends_on('mpi')`, you can do `spec['mpi']` to see what it was
  concretized to.  This should simplify MPI and BLAS packages.

      'mpi' in spec
      'blas' in spec

  Previously, if the spec had been concretized, these would be `False`
  because there was not a dependency in the DAG with either of these
  names. These will now be `True` even if the spec has been
  concretized.  So, e.g., this will print "YES"

      s = Spec('callpath ^mpich')
      if 'mpi' in spec:
          print "YES"

- Similarly, this will be True:

      Spec('mpich').satisfies('mpi')

- Because of the way virtual dependencies are currently implemented,
  the above required some fiddling around with `package.py` so that it
  would never call `Spec.__contains__` (and result in endless
  recursion).

- This should be fixed by allowing virutal dependnecies to have their
  own package class.
  - This would allow a quicker check for vdeps, without a call to
    `all_packages`.
  - For the time being, `package.py` shouldn't call `__contains__`

lib/spack/spack/package.py
@@ -471,13 +471,18 @@ class Package(object):
         """Spec of the extendee of this package, or None if it is not an extension."""
         if not self.extendees:
             return None
-        name = next(iter(self.extendees))
-        if not name in self.spec:
-            spec, kwargs = self.extendees[name]
-            return spec
 
-        # Need to do this to get the concrete version of the spec
-        return self.spec[name]
+        # TODO: allow more than one extendee.
+        name = next(iter(self.extendees))
+
+        # If the extendee is in the spec's deps already, return that.
+        for dep in self.spec.traverse():
+            if name == dep.name:
+                return dep
+
+        # Otherwise return the spec from the extends() directive
+        spec, kwargs = self.extendees[name]
+        return spec
 
 
     @property
@@ -542,7 +547,7 @@ class Package(object):
 
     def provides(self, vpkg_name):
         """True if this package provides a virtual package with the specified name."""
-        return vpkg_name in self.provided
+        return any(s.name == vpkg_name for s in self.provided)
 
 
     def virtual_dependencies(self, visited=None):
@@ -561,8 +566,11 @@ class Package(object):
            on this one."""
         dependents = []
         for spec in spack.db.installed_package_specs():
-            if self.name != spec.name and self.spec in spec:
-                dependents.append(spec)
+            if self.name == spec.name:
+                continue
+            for dep in spec.traverse():
+                if spec == dep:
+                    dependents.append(spec)
         return dependents
 
 
@@ -985,10 +993,13 @@ class Package(object):
 
             activated = spack.install_layout.extension_map(self.extendee_spec)
             for name, aspec in activated.items():
-                if aspec != self.spec and self.spec in aspec:
-                    raise ActivationError(
-                        "Cannot deactivate %s beacuse %s is activated and depends on it."
-                        % (self.spec.short_spec, aspec.short_spec))
+                if aspec == self.spec:
+                    continue
+                for dep in aspec.traverse():
+                    if self.spec == dep:
+                        raise ActivationError(
+                            "Cannot deactivate %s beacuse %s is activated and depends on it."
+                            % (self.spec.short_spec, aspec.short_spec))
 
         self.extendee_spec.package.deactivate(self, **self.extendee_args)
 

lib/spack/spack/spec.py
@@ -498,7 +498,13 @@ class Spec(object):
            Possible idea: just use conventin and make virtual deps all
            caps, e.g., MPI vs mpi.
         """
-        return not spack.db.exists(self.name)
+        return Spec.is_virtual(self.name)
+
+
+    @staticmethod
+    def is_virtual(name):
+        """Test if a name is virtual without requiring a Spec."""
+        return not spack.db.exists(name)
 
 
     @property
@@ -1224,7 +1230,17 @@ class Spec(object):
         """
         other = self._autospec(other)
 
-        # First thing we care about is whether the name matches
+        # A concrete provider can satisfy a virtual dependency.
+        if not self.virtual and other.virtual:
+            pkg = spack.db.get(self.name)
+            if pkg.provides(other.name):
+                for provided, when_spec in pkg.provided.items():
+                    if self.satisfies(when_spec, deps=False, strict=strict):
+                        if provided.satisfies(other):
+                            return True
+            return False
+
+        # Otherwise, first thing we care about is whether the name matches
         if self.name != other.name:
             return False
 
@@ -1364,10 +1380,20 @@ class Spec(object):
 
 
     def __getitem__(self, name):
-        """TODO: reconcile __getitem__, _add_dependency, __contains__"""
+        """Get a dependency from the spec by its name."""
         for spec in self.traverse():
             if spec.name == name:
                 return spec
+
+        if Spec.is_virtual(name):
+            # TODO: this is a kind of kludgy way to find providers
+            # TODO: should we just keep virtual deps in the DAG instead of
+            # TODO: removing them on concretize?
+            for spec in self.traverse():
+                if spec.virtual:
+                    continue
+                if spec.package.provides(name):
+                    return spec
 
         raise KeyError("No spec with name %s in %s" % (name, self))
 
@@ -1380,6 +1406,7 @@ class Spec(object):
         for s in self.traverse():
             if s.satisfies(spec, strict=True):
                 return True
+
         return False
 
 

lib/spack/spack/test/concretize.py
@@ -152,7 +152,10 @@ class ConcretizeTest(MockPackagesTest):
         spec.concretize()
 
         self.assertTrue('zmpi' in spec.dependencies)
-        self.assertFalse('mpi' in spec)
+        self.assertTrue(all(not 'mpi' in d.dependencies for d in spec.traverse()))
+        self.assertTrue('zmpi' in spec)
+        self.assertTrue('mpi' in spec)
+
         self.assertTrue('fake' in spec.dependencies['zmpi'])
 
 
@@ -168,7 +171,9 @@ class ConcretizeTest(MockPackagesTest):
         self.assertTrue('zmpi' in spec.dependencies['callpath'].dependencies)
         self.assertTrue('fake' in spec.dependencies['callpath'].dependencies['zmpi'].dependencies)
 
-        self.assertFalse('mpi' in spec)
+        self.assertTrue(all(not 'mpi' in d.dependencies for d in spec.traverse()))
+        self.assertTrue('zmpi' in spec)
+        self.assertTrue('mpi' in spec)
 
 
     def test_my_dep_depends_on_provider_of_my_virtual_dep(self):

lib/spack/spack/test/spec_semantics.py
@@ -189,6 +189,67 @@ class SpecSematicsTest(MockPackagesTest):
         self.check_unsatisfiable('mpich+foo', 'mpich~foo')
 
 
+    def test_satisfies_virtual(self):
+        self.assertTrue(Spec('mpich').satisfies(Spec('mpi')))
+        self.assertTrue(Spec('mpich2').satisfies(Spec('mpi')))
+        self.assertTrue(Spec('zmpi').satisfies(Spec('mpi')))
+
+
+    # ================================================================================
+    # Indexing specs
+    # ================================================================================
+    def test_self_index(self):
+        s = Spec('callpath')
+        self.assertTrue(s['callpath'] == s)
+
+
+    def test_dep_index(self):
+        s = Spec('callpath')
+        s.normalize()
+
+        self.assertTrue(s['callpath'] == s)
+        self.assertTrue(type(s['dyninst']) == Spec)
+        self.assertTrue(type(s['libdwarf']) == Spec)
+        self.assertTrue(type(s['libelf']) == Spec)
+        self.assertTrue(type(s['mpi']) == Spec)
+
+        self.assertTrue(s['dyninst'].name  == 'dyninst')
+        self.assertTrue(s['libdwarf'].name == 'libdwarf')
+        self.assertTrue(s['libelf'].name   == 'libelf')
+        self.assertTrue(s['mpi'].name      == 'mpi')
+
+
+    def test_spec_contains_deps(self):
+        s = Spec('callpath')
+        s.normalize()
+        self.assertTrue('dyninst' in s)
+        self.assertTrue('libdwarf' in s)
+        self.assertTrue('libelf' in s)
+        self.assertTrue('mpi' in s)
+
+
+    def test_virtual_index(self):
+        s = Spec('callpath')
+        s.concretize()
+
+        s_mpich = Spec('callpath ^mpich')
+        s_mpich.concretize()
+
+        s_mpich2 = Spec('callpath ^mpich2')
+        s_mpich2.concretize()
+
+        s_zmpi = Spec('callpath ^zmpi')
+        s_zmpi.concretize()
+
+
+        self.assertTrue(s['mpi'].name != 'mpi')
+        self.assertTrue(s_mpich['mpi'].name == 'mpich')
+        self.assertTrue(s_mpich2['mpi'].name == 'mpich2')
+        self.assertTrue(s_zmpi['zmpi'].name == 'zmpi')
+
+        for spec in [s, s_mpich, s_mpich2, s_zmpi]:
+            self.assertTrue('mpi' in spec)
+
 
     # ================================================================================
     # Constraints

commit eb54692de6e930d3ac1615fb33f3fc4392e2600f

Fix bug in uninstall (regression in e06c974)

lib/spack/spack/cmd/uninstall.py
@@ -22,13 +22,17 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+import sys
 from external import argparse
 
 import llnl.util.tty as tty
+from llnl.util.tty.colify import colify
 
 import spack
 import spack.cmd
 import spack.packages
+from spack.cmd.find import display_specs
+from spack.package import PackageStillNeededError
 
 description="Remove an installed package"
 
@@ -57,13 +61,14 @@ def uninstall(parser, args):
     for spec in specs:
         matching_specs = spack.db.get_installed(spec)
         if not args.all and len(matching_specs) > 1:
-            args =  ["%s matches multiple packages." % spec,
-                     "Matching packages:"]
-            args += ["  " + str(s) for s in matching_specs]
-            args += ["You can either:",
-                     "  a) Use spack uninstall -a to uninstall ALL matching specs, or",
-                     "  b) use a more specific spec."]
-            tty.die(*args)
+            tty.error("%s matches multiple packages:" % spec)
+            print
+            display_specs(matching_specs, long=True)
+            print
+            print "You can either:"
+            print "  a) Use a more specific spec, or"
+            print "  b) use spack uninstall -a to uninstall ALL matching specs."
+            sys.exit(1)
 
         if len(matching_specs) == 0:
             if args.force: continue
@@ -86,4 +91,13 @@ def uninstall(parser, args):
 
     # Uninstall packages in order now.
     for pkg in pkgs:
-        pkg.do_uninstall(force=args.force)
+        try:
+            pkg.do_uninstall(force=args.force)
+        except PackageStillNeededError, e:
+            tty.error("Will not uninstall %s" % e.spec.format("$_$@$%@$#", color=True))
+            print
+            print "The following packages depend on it:"
+            display_specs(e.dependents, long=True)
+            print
+            print "You can use spack uninstall -f to force this action."
+            sys.exit(1)

lib/spack/spack/package.py
@@ -569,7 +569,7 @@ class Package(object):
             if self.name == spec.name:
                 continue
             for dep in spec.traverse():
-                if spec == dep:
+                if self.spec == dep:
                     dependents.append(spec)
         return dependents
 
@@ -897,12 +897,9 @@ class Package(object):
             raise InstallError(str(self.spec) + " is not installed.")
 
         if not force:
-            deps = self.installed_dependents
-            formatted_deps = [s.format('$_$@$%@$+$=$#') for s in deps]
-            if deps: raise InstallError(
-                "Cannot uninstall %s." % self.spec,
-                "The following installed packages depend on it: %s" %
-                ' '.join(formatted_deps))
+            dependents = self.installed_dependents
+            if dependents:
+                raise PackageStillNeededError(self.spec, dependents)
 
         # Pre-uninstall hook runs first.
         spack.hooks.pre_uninstall(self)
@@ -1180,6 +1177,15 @@ class InstallError(spack.error.SpackError):
     """Raised when something goes wrong during install or uninstall."""
     def __init__(self, message, long_msg=None):
         super(InstallError, self).__init__(message, long_msg)
+
+
+class PackageStillNeededError(InstallError):
+    """Raised when package is still needed by another on uninstall."""
+    def __init__(self, spec, dependents):
+        super(PackageStillNeededError, self).__init__(
+            "Cannot uninstall %s" % spec)
+        self.spec = spec
+        self.dependents = dependents
 
 
 class PackageError(spack.error.SpackError):