d92b1fd30a4a46dc1cf6eb2424b4529c3ec7815d
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-10-16 19:08:25 +0200

    Write adaptors for CompilerSpec and Compiler
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/package_base.py
+++ lib/spack/spack/package_base.py
@@ -53,6 +53,7 @@ import spack.util.environment
 import spack.util.path
 import spack.util.web
 import spack.variant
+from spack.compilers.adaptor import DeprecatedCompiler
 from spack.error import InstallError, NoURLError, PackageError
 from spack.filesystem_view import YamlFilesystemView
 from spack.resource import Resource
@@ -67,9 +68,8 @@ FLAG_HANDLER_RETURN_TYPE = Tuple[
 ]
 FLAG_HANDLER_TYPE = Callable[[str, Iterable[str]], FLAG_HANDLER_RETURN_TYPE]
 
-"""Allowed URL schemes for spack packages."""
+#: Allowed URL schemes for spack packages
 _ALLOWED_URL_SCHEMES = ["http", "https", "ftp", "file", "git"]
-
 
 #: Filename for the Spack build/install log.
 _spack_build_logfile = "spack-build-out.txt"
@@ -591,6 +591,8 @@ class PackageBase(WindowsRPath, PackageViewMixin, metaclass=PackageMeta):
     specific build systems.
 
     """
+
+    compiler = DeprecatedCompiler()
 
     #
     # These are default values for instance variables.
@@ -1488,14 +1490,6 @@ class PackageBase(WindowsRPath, PackageViewMixin, metaclass=PackageMeta):
     @property
     def home(self):
         return self.prefix
-
-    @property  # type: ignore[misc]
-    @memoized
-    def compiler(self):
-        """Get the spack.compiler.Compiler object used to build this package"""
-        if not self.spec.concrete:
-            raise ValueError("Can only get a compiler for a concrete package.")
-        raise NotImplementedError("Wrapper to old API still to be implemented")
 
     def url_version(self, version):
         """
