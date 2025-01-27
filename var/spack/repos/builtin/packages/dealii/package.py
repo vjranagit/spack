64003581fb73ab9205810f1473afb0cd6a9351f5
Harmen Stoppels <me@harmenstoppels.nl>
2025-01-27 23:00:32 +0100

    dealii: add vtk backward compat bound (#48744)

--- var/spack/repos/builtin/packages/dealii/package.py
+++ var/spack/repos/builtin/packages/dealii/package.py
@@ -258,7 +258,7 @@ class Dealii(CMakePackage, CudaPackage):
         arch_str = f"+cuda cuda_arch={_arch}"
         trilinos_spec = f"trilinos +wrapper {arch_str}"
         depends_on(trilinos_spec, when=f"@9.5:+trilinos {arch_str}")
-    depends_on("vtk", when="@9.6:+vtk")
+    depends_on("vtk@9:", when="@9.6:+vtk")
 
     # Explicitly provide a destructor in BlockVector,
     # otherwise deal.II may fail to build with Intel compilers.
