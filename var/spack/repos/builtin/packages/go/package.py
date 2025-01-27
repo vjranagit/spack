91921d2f4b92061bc0decad7afd8108ae574aef6
Alec Scott <hi@alecbcs.com>
2025-01-27 14:32:56 -0500

    Go build system: reduce resulting installation sizes (#47943)
    * Reduce the size of outputted go built binaries
    * Remove unused import from go package
    * go: remove comment from setup dependents build env
    * Add back missing imports after rebase

--- var/spack/repos/builtin/packages/go/package.py
+++ var/spack/repos/builtin/packages/go/package.py
@@ -5,8 +5,6 @@
 import os
 import re
 
-from llnl.util import tty
-
 from spack.package import *
 
 # - vanilla CentOS 7, and possibly other systems, fail a test:
@@ -119,30 +117,8 @@ class Go(Package):
     def setup_dependent_package(self, module, dependent_spec):
         """Called before go modules' build(), install() methods.
 
-        In most cases, extensions will only need to set GOPATH and use go::
-
-        env['GOPATH'] = self.source_path + ':' + env['GOPATH']
         go('get', '<package>', env=env)
         install_tree('bin', prefix.bin)
         """
         #  Add a go command/compiler for extensions
         module.go = self.spec["go"].command
-
-    def generate_path_components(self, dependent_spec):
-        if os.environ.get("GOROOT", False):
-            tty.warn("GOROOT is set, this is not recommended")
-
-        # Set to include paths of dependencies
-        path_components = [dependent_spec.prefix]
-        for d in dependent_spec.traverse():
-            if d.package.extends(self.spec):
-                path_components.append(d.prefix)
-        return ":".join(path_components)
-
-    def setup_dependent_build_environment(self, env, dependent_spec):
-        # This *MUST* be first, this is where new code is installed
-        env.prepend_path("GOPATH", self.generate_path_components(dependent_spec))
-
-    def setup_dependent_run_environment(self, env, dependent_spec):
-        # Allow packages to find this when using module files
-        env.prepend_path("GOPATH", self.generate_path_components(dependent_spec))
