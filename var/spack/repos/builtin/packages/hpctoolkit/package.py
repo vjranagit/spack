8bd58d06449b732b48712ac888ebebd8d7bebdae
Jonathon Anderson <17242663+blue42u@users.noreply.github.com>
2025-01-29 11:26:54 -0600

    hpctoolkit: Add `+docs` variant and manpages (#48566)
    * py-mdit-py-plugins: Add new versions 0.3.5, 0.4.2
      Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>
    * py-myst-parser: Add new versions 0.19.0 to 4.0.0
      Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>
    * hpctoolkit: Add +docs variant and manpages
       This commit unconditionally enables manpages for the HPCToolkit tools.
       The new `+docs` variant enables additional documentation, specifically
       the user's manual. Both require new build-time dependencies.
      Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>
    
    ---------
    
    Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>

--- var/spack/repos/builtin/packages/hpctoolkit/package.py
+++ var/spack/repos/builtin/packages/hpctoolkit/package.py
@@ -118,6 +118,12 @@ class Hpctoolkit(AutotoolsPackage, MesonPackage):
         when="build_system=autotools",
     )
     variant("viewer", default=True, description="Include hpcviewer.")
+    variant(
+        "docs",
+        default=False,
+        description="Include extra documentation (user's manual)",
+        when="@develop",
+    )
 
     variant(
         "python", default=False, description="Support unwinding Python source.", when="@2023.03:"
@@ -184,6 +190,10 @@ class Hpctoolkit(AutotoolsPackage, MesonPackage):
 
     depends_on("zlib-api")
     depends_on("zlib+shared", when="^[virtuals=zlib-api] zlib")
+
+    depends_on("py-docutils", type="build", when="@develop")
+    depends_on("py-sphinx", type="build", when="+docs")
+    depends_on("py-myst-parser@0.19:", type="build", when="+docs")
 
     depends_on("cuda", when="+cuda")
     depends_on("oneapi-level-zero", when="+level_zero")
@@ -402,6 +412,8 @@ class MesonBuilder(spack.build_systems.meson.MesonBuilder):
         spec = self.spec
 
         args = [
+            "-Dmanpages=enabled",
+            "-Dmanual=" + ("enabled" if spec.satisfies("+docs") else "disabled"),
             "-Dhpcprof_mpi=" + ("enabled" if spec.satisfies("+mpi") else "disabled"),
             "-Dpython=" + ("enabled" if spec.satisfies("+python") else "disabled"),
             "-Dpapi=" + ("enabled" if spec.satisfies("+papi") else "disabled"),
