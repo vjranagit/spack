4f9d7b43e1d229933276b04a9a4429a775eee1d1
Harmen Stoppels <me@harmenstoppels.nl>
2025-02-06 09:55:27 +0100

    spec.py: ensure == is false if true modulo precomputed dag hash (#48889)

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -3855,6 +3855,13 @@ class Spec:
         for item in self._cmp_node():
             yield item
 
+        # If there is ever a breaking change to hash computation, whether accidental or purposeful,
+        # two specs can be identical modulo DAG hash, depending on what time they were concretized
+        # From the perspective of many operation in Spack (database, build cache, etc) a different
+        # DAG hash means a different spec. Here we ensure that two otherwise identical specs, one
+        # serialized before the hash change and one after, are considered different.
+        yield self.dag_hash() if self.concrete else None
+
         # This needs to be in _cmp_iter so that no specs with different process hashes
         # are considered the same by `__hash__` or `__eq__`.
         #
