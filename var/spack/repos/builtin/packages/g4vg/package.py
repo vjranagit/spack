62ffd83fc7d67411c6da2cde4dd41c8c843b4b67
Seth R. Johnson <johnsonsr@ornl.gov>
2025-02-12 11:04:42 -0500

    g4vg: add 'develop' branch (#49003)
    * g4vg: add develop version
    
    * celeritas: add develop version
    
    * Fix style
    
    * REVERTME: move celeritas changes to another branch
    
    * Get full repo
    
    * remove unneeded variable

--- var/spack/repos/builtin/packages/g4vg/package.py
+++ var/spack/repos/builtin/packages/g4vg/package.py
@@ -9,11 +9,14 @@ class G4vg(CMakePackage):
     """Generate VecGeom geometry representations from in-memory Geant4 geometry."""
 
     homepage = "https://github.com/celeritas-project/g4vg"
+    git = "https://github.com/celeritas-project/g4vg.git"
     url = "https://github.com/celeritas-project/g4vg/releases/download/v1.0.1/g4vg-1.0.1.tar.gz"
 
     maintainers("sethrj", "drbenmorgan")
 
     license("Apache-2.0", checked_by="sethrj")
+
+    version("develop", branch="main", get_full_repo=True)
 
     version("1.0.1", sha256="add7ce4bc37889cac2101323a997cea8574b18da6cbeffdab44a2b714d134e99")
 
