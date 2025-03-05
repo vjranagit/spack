2698ab51a38d415d73c33f1a22956d871ef921ad
Harmen Stoppels <me@harmenstoppels.nl>
2025-03-05 15:42:48 +0100

    Define Package API version (#49274)
    Defines `spack.package_api_version` and `spack.min_package_api_version`
    as tuples (major, minor).
    
    This defines resp. the current Package API version implemented by this version
    of Spack and the minimal Package API version it is backwards compatible with.
    
    Repositories can optionally define:
    ```yaml
    repo:
        namespace: my_repo
        api: v1.2
    ```
    which indicates they are compatible with versions of Spack that implement
    Package API `>= 1.2` and `< 2.0`. When the `api` key is omitted, the default
    `v1.0` is assumed.

--- lib/spack/spack/__init__.py
+++ lib/spack/spack/__init__.py
@@ -14,6 +14,18 @@ import spack.util.git
 __version__ = "0.23.2.dev0"
 spack_version = __version__
 
+#: The current Package API version implemented by this version of Spack. The Package API defines
+#: the Python interface for packages as well as the layout of package repositories. The minor
+#: version is incremented when the package API is extended in a backwards-compatible way. The major
+#: version is incremented upon breaking changes. This version is changed independently from the
+#: Spack version.
+package_api_version = (1, 0)
+
+#: The minimum Package API version that this version of Spack is compatible with. This should
+#: always be a tuple of the form ``(major, 0)``, since compatibility with vX.Y implies
+#: compatibility with vX.0.
+min_package_api_version = (1, 0)
+
 
 def __try_int(v):
     try:
@@ -80,4 +92,6 @@ __all__ = [
     "get_version",
     "get_spack_commit",
     "get_short_version",
+    "package_api_version",
+    "min_package_api_version",
 ]
