8841e24e6768b7cd16ae3e884977f5e71bfae13a
Tamara Dahlgren <35777542+tldahlgren@users.noreply.github.com>
2025-03-09 19:33:44 -0700

    Support independent includes with conditional, optional, and remote entries (#48784)
    Supersedes #46792.
    Closes #40018.
    Closes #31026.
    Closes #2700.
    
    There were a number of feature requests for os-specific config. This enables os-specific
    config without adding a lot of special sub-scopes.
    
    Support `include:` as an independent configuration schema, allowing users to include
    configuration scopes from files or directories. Includes can be:
    * conditional (similar to definitions in environments), and/or
    * optional (i.e., the include will be skipped if it does not exist).
    
    Includes can be paths or URLs (`ftp`, `https`, `http` or `file`). Paths can be absolute or
    relative . Environments can include configuration files using the same schema. Remote includes 
    must be checked by `sha256`.
    
    Includes can also be recursive, and this modifies the config system accordingly so that
    we push included configuration scopes on the stack *before* their including scopes, and
    we remove configuration scopes from the stack when their including scopes are removed.
    
    For example, you could have an `include.yaml` file (e.g., under `$HOME/.spack`) to specify
    global includes:
    
    ```
    include:
    - ./enable_debug.yaml
    - path: https://github.com/spack/spack-configs/blob/main/NREL/configs/mac/config.yaml
      sha256: 37f982915b03de18cc4e722c42c5267bf04e46b6a6d6e0ef3a67871fcb1d258b
    ```
    
    Or an environment `spack.yaml`:
    
    ```
    spack:
      include:
      - path: "/path/to/a/config-dir-or-file"
        when: os == "ventura"
      - ./path/relative/to/containing/file/that/is/required
      - path: "/path/with/spack/variables/$os/$target"
        optional: true
      - path: https://raw.githubusercontent.com/spack/spack-configs/refs/heads/main/path/to/required/raw/config.yaml
        sha256: 26e871804a92cd07bb3d611b31b4156ae93d35b6a6d6e0ef3a67871fcb1d258b
    ```
    
    Updated TODO:
    - [x] Get existing unit tests to pass with Todd's changes
    - [x] Resolve new (or old) circular imports
    - [x] Ensure remote includes (global) work
    - [x] Ensure remote includes for environments work (note: caches remote
          files under user cache root)
    - [x] add sha256 field to include paths, validate, and require for remote includes
    - [x] add sha256 remote file unit tests
    - [x] revisit how diamond includes should work
    - [x] support recursive includes
    - [x] add recursive include unit tests
    - [x] update docs and unit test to indicate ordering of recursive includes with
          conflicting options is deferred to follow-on work
    
    ---------
    
    Signed-off-by: Todd Gamblin <tgamblin@llnl.gov>
    Co-authored-by: Peter Scheibel <scheibel1@llnl.gov>
    Co-authored-by: Todd Gamblin <tgamblin@llnl.gov>

--- lib/spack/spack/test/bindist.py
+++ lib/spack/spack/test/bindist.py
@@ -200,7 +200,11 @@ else:
 @pytest.mark.requires_executables(*required_executables)
 @pytest.mark.maybeslow
 @pytest.mark.usefixtures(
-    "default_config", "cache_directory", "install_dir_default_layout", "temporary_mirror"
+    "default_config",
+    "cache_directory",
+    "install_dir_default_layout",
+    "temporary_mirror",
+    "mutable_mock_env_path",
 )
 def test_default_rpaths_create_install_default_layout(temporary_mirror_dir):
     """
@@ -272,7 +276,11 @@ def test_default_rpaths_install_nondefault_layout(temporary_mirror_dir):
 @pytest.mark.maybeslow
 @pytest.mark.nomockstage
 @pytest.mark.usefixtures(
-    "default_config", "cache_directory", "install_dir_default_layout", "temporary_mirror"
+    "default_config",
+    "cache_directory",
+    "install_dir_default_layout",
+    "temporary_mirror",
+    "mutable_mock_env_path",
 )
 def test_relative_rpaths_install_default_layout(temporary_mirror_dir):
     """
