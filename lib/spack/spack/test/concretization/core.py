vjranagit/spack

commit 4e0a0d7284537b999674eba646d352f000191ff6

Do not attach dependency types to `%` by default (#50565)
Following #49808 this PR removes the default dependency type attached to the % sigil, 
so that ^ and % have a symmetric behavior.

Modifications:
* Modify Spec.satisfies to account for different deptypes attached to %
* Modify the solver to avoid assuming % refers to a "build only" deptype
* Allow solving %[deptypes=link] mpich etc.

Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

lib/spack/spack/solver/concretize.lp
@@ -183,11 +183,12 @@ literal_node(Root, node(min_dupe_id, Root)) :-  mentioned_in_literal(Root, Root)
   mentioned_in_literal(Root, Mentioned), Mentioned != Root,
   internal_error("must have exactly one condition_set for literals").
 
-1 { build_dependency_of_literal_node(LiteralNode, node(0..Y-1, BuildDependency)) : max_dupes(BuildDependency, Y) } 1 :-
+build_dependency_of_literal_node(LiteralNode, node(X, BuildDependency)) :-
   literal_node(Root, LiteralNode),
   build(LiteralNode),
   not external(LiteralNode),
-  attr("direct_dependency", LiteralNode, build_requirement("node", BuildDependency)).
+  build_requirement(LiteralNode, node(X, BuildDependency)),
+  attr("direct_dependency", LiteralNode, node_requirement("node", BuildDependency)).
 
 condition_set(node(min_dupe_id, Root), LiteralNode) :- literal_node(Root, LiteralNode).
 condition_set(LiteralNode, BuildNode) :- build_dependency_of_literal_node(LiteralNode, BuildNode).
@@ -511,77 +512,77 @@ attr("concrete_variant_set", node(X, A1), Variant, Value, ID)
 
 % If the parent is built, then we have a build_requirement on another node. For concrete nodes,
 % or external nodes, we don't since we are trimming their build dependencies.
-1 { attr("depends_on",  node(X, Parent), node(0..Y-1, BuildDependency), "build") : max_dupes(BuildDependency, Y) } 1
-  :- attr("direct_dependency", node(X, Parent), build_requirement("node", BuildDependency)),
-     build(node(X, Parent)),
-     not external(node(X, Parent)).
 
 % Concrete nodes
-:- attr("direct_dependency", ParentNode, build_requirement("node", BuildDependency)),
+:- attr("direct_dependency", ParentNode, node_requirement("node", BuildDependency)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    concrete(ParentNode),
    not attr("concrete_build_dependency", ParentNode, BuildDependency, _).
 
-:- attr("direct_dependency", ParentNode, build_requirement("node_version_satisfies", BuildDependency, Constraint)),
+:- attr("direct_dependency", ParentNode, node_requirement("node_version_satisfies", BuildDependency, Constraint)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
    not 1 { pkg_fact(BuildDependency, version_satisfies(Constraint, Version)) : hash_attr(BuildDependencyHash, "version", BuildDependency, Version) } 1.
 
-:- attr("direct_dependency", ParentNode, build_requirement("provider_set", BuildDependency, Virtual)),
+:- attr("direct_dependency", ParentNode, node_requirement("provider_set", BuildDependency, Virtual)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
    attr("virtual_on_build_edge", ParentNode, BuildDependency, Virtual),
    not 1 { pkg_fact(BuildDependency, version_satisfies(Constraint, Version)) : hash_attr(BuildDependencyHash, "version", BuildDependency, Version) } 1.
 
 error(100, "Cannot satisfy the request on {0} to have {1}={2}", BuildDependency, Variant, Value)
-:- attr("direct_dependency", ParentNode, build_requirement("variant_set", BuildDependency, Variant, Value)),
+:- attr("direct_dependency", ParentNode, node_requirement("variant_set", BuildDependency, Variant, Value)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
    not hash_attr(BuildDependencyHash, "variant_value", BuildDependency, Variant, Value).
 
 error(100, "Cannot satisfy the request on {0} to have the target set to {1}", BuildDependency, Target)
-:- attr("direct_dependency", ParentNode, build_requirement("node_target_set", BuildDependency, Target)),
+:- attr("direct_dependency", ParentNode, node_requirement("node_target_set", BuildDependency, Target)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
    not hash_attr(BuildDependencyHash, "node_target", BuildDependency, Target).
 
 error(100, "Cannot satisfy the request on {0} to have the os set to {1}", BuildDependency, NodeOS)
-:- attr("direct_dependency", ParentNode, build_requirement("node_os_set", BuildDependency, NodeOS)),
+:- attr("direct_dependency", ParentNode, node_requirement("node_os_set", BuildDependency, NodeOS)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
    not hash_attr(BuildDependencyHash, "node_os", BuildDependency, NodeOS).
 
 error(100, "Cannot satisfy the request on {0} to have the platform set to {1}", BuildDependency, Platform)
-:- attr("direct_dependency", ParentNode, build_requirement("node_platform_set", BuildDependency, Platform)),
+:- attr("direct_dependency", ParentNode, node_requirement("node_platform_set", BuildDependency, Platform)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
    not hash_attr(BuildDependencyHash, "node_platform", BuildDependency, Platform).
 
 error(100, "Cannot satisfy the request on {0} to have the following hash {1}", BuildDependency, BuildHash)
-:- attr("direct_dependency", ParentNode, build_requirement("node_target_set", BuildDependency, Target)),
+:- attr("direct_dependency", ParentNode, node_requirement("node_target_set", BuildDependency, Target)),
+   concrete_build_requirement(ParentNode, BuildDependency),
    attr("concrete_build_dependency", ParentNode, BuildDependency, BuildDependencyHash),
-   attr("direct_dependency", ParentNode, build_requirement("hash", BuildDependency, BuildHash)),
+   attr("direct_dependency", ParentNode, node_requirement("hash", BuildDependency, BuildHash)),
    BuildHash != BuildDependencyHash.
 
 % External nodes
-:- attr("direct_dependency", ParentNode, build_requirement("node", BuildDependency)),
+:- attr("direct_dependency", ParentNode, node_requirement("node", BuildDependency)),
    external(ParentNode),
-   not attr("external_build_requirement", ParentNode, build_requirement("node", BuildDependency)).
+   not attr("external_build_requirement", ParentNode, node_requirement("node", BuildDependency)).
 
 candidate_external_version(Constraint, BuildDependency, Version)
-  :- attr("direct_dependency", ParentNode, build_requirement("node_version_satisfies", BuildDependency, Constraint)),
+  :- attr("direct_dependency", ParentNode, node_requirement("node_version_satisfies", BuildDependency, Constraint)),
      external(ParentNode),
      pkg_fact(BuildDependency, version_satisfies(Constraint, Version)).
 
 error(100, "External {0} cannot satisfy both {1} and {2}", BuildDependency, LiteralConstraint, ExternalConstraint)
-  :- attr("direct_dependency", ParentNode, build_requirement("node_version_satisfies", BuildDependency, LiteralConstraint)),
+  :- attr("direct_dependency", ParentNode, node_requirement("node_version_satisfies", BuildDependency, LiteralConstraint)),
      external(ParentNode),
-     attr("external_build_requirement", ParentNode, build_requirement("node_version_satisfies", BuildDependency, ExternalConstraint)),
+     attr("external_build_requirement", ParentNode, node_requirement("node_version_satisfies", BuildDependency, ExternalConstraint)),
      not 1 { pkg_fact(BuildDependency, version_satisfies(ExternalConstraint, Version)) : candidate_external_version(LiteralConstraint, BuildDependency, Version) }.
 
 
 % Asking for gcc@10 %gcc@9 shouldn't give us back an external gcc@10, just because of the hack
 % we have on externals
-:- attr("direct_dependency", node(X, Parent), build_requirement("node", BuildDependency)),
+:- attr("direct_dependency", node(X, Parent), node_requirement("node", BuildDependency)),
    Parent == BuildDependency,
    external(node(X, Parent)).
-
-build_requirement(node(X, Parent), node(Y, BuildDependency)) :-
-  attr("depends_on", node(X, Parent), node(Y, BuildDependency), "build"),
-  attr("direct_dependency", node(X, Parent), build_requirement("node", BuildDependency)).
 
 1 { virtual_build_requirement(ParentNode, node(0..Y-1, Virtual)) : max_dupes(Virtual, Y) } 1
   :- attr("dependency_holds", ParentNode, Virtual, "build"),
@@ -599,7 +600,7 @@ build_requirement(ParentNode, ProviderNode) :- virtual_build_requirement(ParentN
 % Adding a "direct_dependency" is a way to discriminate between the incompatible
 % version constraints on "gcc" in the "imposed_constraint".
 attr("node_version_satisfies", node(X, BuildDependency), Constraint) :-
-  attr("direct_dependency", ParentNode, build_requirement("node_version_satisfies", BuildDependency, Constraint)),
+  attr("direct_dependency", ParentNode, node_requirement("node_version_satisfies", BuildDependency, Constraint)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 % Account for properties on the build requirements
@@ -607,30 +608,30 @@ attr("node_version_satisfies", node(X, BuildDependency), Constraint) :-
 % root %gcc@12.0 <properties for gcc> ^dep
 %
 attr("variant_set", node(X, BuildDependency), Variant, Value) :-
-  attr("direct_dependency", ParentNode, build_requirement("variant_set", BuildDependency, Variant, Value)),
+  attr("direct_dependency", ParentNode, node_requirement("variant_set", BuildDependency, Variant, Value)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 attr("depends_on",  node(X, Parent), node(Y, BuildDependency), "build") :- build_requirement(node(X, Parent), node(Y, BuildDependency)).
 
 attr("node_target_set", node(X, BuildDependency), Target) :-
-  attr("direct_dependency", ParentNode, build_requirement("node_target_set", BuildDependency, Target)),
+  attr("direct_dependency", ParentNode, node_requirement("node_target_set", BuildDependency, Target)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 attr("node_os_set", node(X, BuildDependency), NodeOS) :-
-  attr("direct_dependency", ParentNode, build_requirement("node_os_set", BuildDependency, NodeOS)),
+  attr("direct_dependency", ParentNode, node_requirement("node_os_set", BuildDependency, NodeOS)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 attr("node_platform_set", node(X, BuildDependency), NodePlatform) :-
-  attr("direct_dependency", ParentNode, build_requirement("node_platform_set", BuildDependency, NodePlatform)),
+  attr("direct_dependency", ParentNode, node_requirement("node_platform_set", BuildDependency, NodePlatform)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 attr("hash", node(X, BuildDependency), BuildHash) :-
-  attr("direct_dependency", ParentNode, build_requirement("hash", BuildDependency, BuildHash)),
+  attr("direct_dependency", ParentNode, node_requirement("hash", BuildDependency, BuildHash)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 
 1 { attr("provider_set", node(X, BuildDependency), node(0..Y-1, Virtual)) : max_dupes(Virtual, Y) } 1 :-
-  attr("direct_dependency", ParentNode, build_requirement("provider_set", BuildDependency, Virtual)),
+  attr("direct_dependency", ParentNode, node_requirement("provider_set", BuildDependency, Virtual)),
   build_requirement(ParentNode, node(X, BuildDependency)).
 
 % Reconstruct virtual dependencies for reused specs
@@ -968,13 +969,13 @@ attr("external_spec_selected", node(ID, Package), LocalIndex) :-
 
 % Account for compiler annotation on externals
 :- not attr("root", ExternalNode),
-   attr("external_build_requirement", ExternalNode, build_requirement("node", Compiler)),
+   attr("external_build_requirement", ExternalNode, node_requirement("node", Compiler)),
    not node_compiler(_, node(_, Compiler)).
 
 1 { attr("node_version_satisfies", node(X, Compiler), Constraint) : node_compiler(_, node(X, Compiler)) }
   :- not attr("root", ExternalNode),
-     attr("external_build_requirement", ExternalNode, build_requirement("node", Compiler)),
-     attr("external_build_requirement", ExternalNode, build_requirement("node_version_satisfies", Compiler, Constraint)).
+     attr("external_build_requirement", ExternalNode, node_requirement("node", Compiler)),
+     attr("external_build_requirement", ExternalNode, node_requirement("node_version_satisfies", Compiler, Constraint)).
 
 % it cannot happen that a spec is external, but none of the external specs
 % conditions hold.

lib/spack/spack/solver/direct_dependency.lp
@@ -1,0 +1,58 @@
+% Copyright Spack Project Developers. See COPYRIGHT file for details.
+%
+% SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+% A direct dependency is either a runtime requirement, or a build requirement
+1 {
+  build_requirement(PackageNode, node(0..X-1, DirectDependency)) : max_dupes(DirectDependency, X);
+  runtime_requirement(PackageNode, node(0..X-1, DirectDependency)) : max_dupes(DirectDependency, X)
+} 1 :- attr("direct_dependency", PackageNode, node_requirement("node", DirectDependency)),
+       not external(PackageNode),
+       not concrete(PackageNode).
+
+1 {
+  concrete_build_requirement(PackageNode, DirectDependency);
+  runtime_requirement(PackageNode, node(0..X-1, DirectDependency)) : max_dupes(DirectDependency, X)
+} 1 :- attr("direct_dependency", PackageNode, node_requirement("node", DirectDependency)),
+       concrete(PackageNode).
+
+%%%%
+% Build requirement
+%%%%
+
+% A build requirement that is not concrete has a "build" only dependency
+attr("depends_on", node(X, Parent), node(Y, BuildDependency), "build") :-
+ build_requirement(node(X, Parent), node(Y, BuildDependency)),
+ build(node(X, Parent)).
+
+% Any other dependency type is forbidden
+:- build_requirement(ParentNode, ChildNode),
+   build(ParentNode),
+   attr("depends_on", ParentNode, ChildNode, Type), Type != "build".
+
+:- concrete_build_requirement(ParentNode, ChildPackage),
+   concrete(ParentNode),
+   attr("depends_on", ParentNode, node(_, ChildPackage), _).
+
+%%%%
+% Runtime requirement
+%%%%
+
+:- runtime_requirement(ParentNode, ChildNode),
+   not 1 { attr("depends_on", ParentNode, ChildNode, "link"); attr("depends_on", ParentNode, ChildNode, "run") }.
+
+attr(AttributeName, node(X, ChildPackage), A1)
+  :- runtime_requirement(ParentNode, node(X, ChildPackage)),
+     attr("direct_dependency", ParentNode, node_requirement(AttributeName, ChildPackage, A1)).
+
+attr(AttributeName, node(X, ChildPackage), A1, A2)
+  :- runtime_requirement(ParentNode, node(X, ChildPackage)),
+     attr("direct_dependency", ParentNode, node_requirement(AttributeName, ChildPackage, A1, A2)).
+
+attr(AttributeName, node(X, ChildPackage), A1, A2, A3)
+  :- runtime_requirement(ParentNode, node(X, ChildPackage)),
+     attr("direct_dependency", ParentNode, node_requirement(AttributeName, ChildPackage, A1, A2, A3)).
+
+attr(AttributeName, node(X, ChildPackage), A1, A2, A3, A4)
+  :- runtime_requirement(ParentNode, node(X, ChildPackage)),
+     attr("direct_dependency", ParentNode, node_requirement(AttributeName, ChildPackage, A1, A2, A3, A4)).

lib/spack/spack/test/concretization/core.py
@@ -119,6 +119,8 @@ def binary_compatibility(monkeypatch, request):
         "conflict",
         "conflict~foo%clang",
         "conflict-parent%gcc",
+        # Direct dependency with different deptypes
+        "mpileaks %[deptypes=link] mpich",
     ]
 )
 def spec(request):
@@ -3701,3 +3703,47 @@ def test_use_compiler_by_hash(mock_packages, mutable_database, mutable_config):
         s = spack.concretize.concretize_one(f"mpileaks %gcc/{installed_spec.dag_hash()}")
 
     assert s["c"].dag_hash() == installed_spec.dag_hash()
+
+
+@pytest.mark.parametrize(
+    "spec_str,expected,not_expected",
+    [
+        # Simple build requirement on gcc, as a provider for c
+        (
+            "mpileaks %gcc",
+            ["%[deptypes=build] gcc"],
+            ["%[deptypes=link] gcc", "%[deptypes=run] gcc"],
+        ),
+        # Require mpich as a direct dependency of mpileaks
+        (
+            "mpileaks %[deptypes=link] mpich",
+            ["%[deptypes=build,link] mpich", "^callpath%[deptypes=build,link] mpich"],
+            ["%[deptypes=run] mpich"],
+        ),
+        (
+            "mpileaks %[deptypes=link] mpich+debug",  # non-default variant
+            ["%[deptypes=build,link] mpich+debug"],
+            ["% mpich~debug"],
+        ),
+        # Require mpich as a direct dependency of two nodes, with compatible constraints
+        (
+            "mpileaks %mpich+debug ^callpath %mpich@3.0.3",  # non-default variant
+            [
+                "%[deptypes=build,link] mpich@3.0.3+debug",
+                "^callpath %[deptypes=build,link] mpich@3.0.3+debug",
+            ],
+            ["%mpich~debug"],
+        ),
+    ],
+)
+def test_specifying_direct_dependencies(
+    spec_str, expected, not_expected, default_mock_concretization
+):
+    """Tests solving % in different scenarios, either for runtime or buildtime dependencies."""
+    concrete_spec = default_mock_concretization(spec_str)
+
+    for c in expected:
+        assert concrete_spec.satisfies(c)
+
+    for c in not_expected:
+        assert not concrete_spec.satisfies(c)

var/spack/repos/spack_repo/builtin/packages/gcc/package.py
@@ -1165,7 +1165,7 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage, CompilerPackage):
         for language in ("c", "cxx", "fortran"):
             pkg("*").depends_on(
                 f"gcc-runtime@{spec.version}:",
-                when=f"%[virtuals={language}] {spec.name}@{spec.versions}",
+                when=f"%[deptypes=build virtuals={language}] {spec.name}@{spec.versions}",
                 type="link",
                 description=f"Inject gcc-runtime when gcc is used as a {language} compiler",
             )
@@ -1179,17 +1179,21 @@ class Gcc(AutotoolsPackage, GNUMirrorPackage, CompilerPackage):
         for fortran_virtual in ("fortran-rt", gfortran_str):
             pkg("*").depends_on(
                 fortran_virtual,
-                when=f"%[virtuals=fortran] {spec.name}@{spec.versions}",
+                when=f"%[deptypes=build virtuals=fortran] {spec.name}@{spec.versions}",
                 type="link",
                 description=f"Add a dependency on '{gfortran_str}' for nodes compiled with "
                 f"{spec} and using the 'fortran' language",
             )
         # The version of gcc-runtime is the same as the %gcc used to "compile" it
-        pkg("gcc-runtime").requires(f"@{spec.versions}", when=f"%{spec.name}@{spec.versions}")
+        pkg("gcc-runtime").requires(
+            f"@{spec.versions}", when=f"%[deptypes=build] {spec.name}@{spec.versions}"
+        )
 
         # If a node used %gcc@X.Y its dependencies must use gcc-runtime@:X.Y
         # (technically @:X is broader than ... <= @=X but this should work in practice)
-        pkg("*").propagate(f"gcc@:{spec.version}", when=f"%{spec.name}@{spec.versions}")
+        pkg("*").propagate(
+            f"gcc@:{spec.version}", when=f"%[deptypes=build] {spec.name}@{spec.versions}"
+        )
 
     def _post_buildcache_install_hook(self):
         if not self.spec.satisfies("platform=linux"):

var/spack/repos/spack_repo/builtin/packages/intel_oneapi_compilers/package.py
@@ -674,7 +674,7 @@ class IntelOneapiCompilers(IntelOneApiPackage, CompilerPackage):
         for language in ("c", "cxx", "fortran"):
             pkg("*").depends_on(
                 f"intel-oneapi-runtime@{spec.version}:",
-                when=f"%[virtuals={language}] {spec.name}@{spec.versions}",
+                when=f"%[deptypes=build virtuals={language}] {spec.name}@{spec.versions}",
                 type="link",
                 description="Inject intel-oneapi-runtime when oneapi is used as "
                 f"a {language} compiler",
@@ -683,20 +683,21 @@ class IntelOneapiCompilers(IntelOneApiPackage, CompilerPackage):
         for fortran_virtual in ("fortran-rt", "libifcore@5"):
             pkg("*").depends_on(
                 fortran_virtual,
-                when=f"%[virtuals=fortran] {spec.name}@{spec.versions}",
+                when=f"%[deptypes=build virtuals=fortran] {spec.name}@{spec.versions}",
                 type="link",
                 description="Add a dependency on 'libifcore' for nodes compiled with "
                 f"{spec.name}@{spec.versions} and using the 'fortran' language",
             )
         # The version of intel-oneapi-runtime is the same as the %oneapi used to "compile" it
         pkg("intel-oneapi-runtime").requires(
-            f"@{spec.versions}", when=f"%{spec.name}@{spec.versions}"
+            f"@{spec.versions}", when=f"%[deptypes=build] {spec.name}@{spec.versions}"
         )
 
         # If a node used %intel-oneapi-runtime@X.Y its dependencies must use @:X.Y
         # (technically @:X is broader than ... <= @=X but this should work in practice)
         pkg("*").propagate(
-            f"intel-oneapi-compilers@:{spec.version}", when=f"%{spec.name}@{spec.versions}"
+            f"intel-oneapi-compilers@:{spec.version}",
+            when=f"%[deptypes=build] {spec.name}@{spec.versions}",
         )
 
     def _cc_path(self):

var/spack/test_repos/spack_repo/builtin_mock/packages/gcc/package.py
@@ -103,7 +103,7 @@ class Gcc(CompilerPackage, Package):
         for language in ("c", "cxx", "fortran"):
             pkg("*").depends_on(
                 f"gcc-runtime@{spec.version}:",
-                when=f"%[virtuals={language}] {spec.name}@{spec.versions}",
+                when=f"%[deptypes=build virtuals={language}] {spec.name}@{spec.versions}",
                 type="link",
                 description=f"Inject gcc-runtime when gcc is used as a {language} compiler",
             )
@@ -117,14 +117,18 @@ class Gcc(CompilerPackage, Package):
         for fortran_virtual in ("fortran-rt", gfortran_str):
             pkg("*").depends_on(
                 fortran_virtual,
-                when=f"%[virtuals=fortran] {spec.name}@{spec.versions}",
+                when=f"%[deptypes=build virtuals=fortran] {spec.name}@{spec.versions}",
                 type="link",
                 description=f"Add a dependency on '{gfortran_str}' for nodes compiled with "
                 f"{spec} and using the 'fortran' language",
             )
         # The version of gcc-runtime is the same as the %gcc used to "compile" it
-        pkg("gcc-runtime").requires(f"@{spec.versions}", when=f"%{spec.name}@{spec.versions}")
+        pkg("gcc-runtime").requires(
+            f"@{spec.versions}", when=f"%[deptypes=build] {spec.name}@{spec.versions}"
+        )
 
         # If a node used %gcc@X.Y its dependencies must use gcc-runtime@:X.Y
         # (technically @:X is broader than ... <= @=X but this should work in practice)
-        pkg("*").propagate(f"gcc@:{spec.version}", when=f"%{spec.name}@{spec.versions}")
+        pkg("*").propagate(
+            f"gcc@:{spec.version}", when=f"%[deptypes=build] {spec.name}@{spec.versions}"
+        )

lib/spack/spack/directives.py
@@ -296,7 +296,7 @@ def _depends_on(
     dependency = deps_by_name.get(spec.name)
 
     edges = spec.edges_to_dependencies()
-    if edges and not all(x.depflag == dt.BUILD for x in edges):
+    if edges and not all(x.direct for x in edges):
         raise DirectiveError(
             f"the '^' sigil cannot be used in 'depends_on' directives. Please reformulate "
             f"the directive below as multiple directives:\n\n"

commit 6cce28af208b44664b098f80d7f5d214ac459685

solver: fix issue when compiler is not a pure build dependency (#51074)
4e0a0d7284537b999674eba646d352f000191ff6 regressed this case by missing the fact that a "provider_set" on the direct dependency must be honored even if the direct dependency turns out to be needed at runtime.

Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

lib/spack/spack/solver/concretize.lp
@@ -667,7 +667,7 @@ attr("hash", node(X, BuildDependency), BuildHash) :-
 
 1 { attr("provider_set", node(X, BuildDependency), node(0..Y-1, Virtual)) : max_dupes(Virtual, Y) } 1 :-
   attr("direct_dependency", ParentNode, node_requirement("provider_set", BuildDependency, Virtual)),
-  build_requirement(ParentNode, node(X, BuildDependency)).
+  direct_dependency(ParentNode, node(X, BuildDependency)).
 
 % Reconstruct virtual dependencies for reused specs
 attr("virtual_on_edge", node(X, A1), node(Y, A2), Virtual)

lib/spack/spack/solver/direct_dependency.lp
@@ -16,6 +16,9 @@
 } 1 :- attr("direct_dependency", PackageNode, node_requirement("node", DirectDependency)),
        concrete(PackageNode).
 
+direct_dependency(ParentNode, ChildNode) :- build_requirement(ParentNode, ChildNode).
+direct_dependency(ParentNode, ChildNode) :- runtime_requirement(ParentNode, ChildNode).
+
 %%%%
 % Build requirement
 %%%%

var/spack/test_repos/spack_repo/builtin_mock/packages/llvm_client/package.py
@@ -1,0 +1,19 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack_repo.builtin_mock.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+
+class LlvmClient(CMakePackage):
+    """A client package that depends on llvm and needs C and C++ compilers."""
+
+    git = "https://github.com/mycpptutorial/helloworld-cmake"
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+
+    version("develop", branch="master")
+
+    depends_on("llvm")

lib/spack/spack/test/concretization/core.py
@@ -3851,6 +3851,18 @@ def test_use_compiler_by_hash(mock_packages, mutable_database, mutable_config):
         ("emacs +native", ["%[virtuals=c deptypes=build,link] gcc"], []),
         ("emacs +native %gcc", ["%[virtuals=c deptypes=build,link] gcc"], []),
         ("emacs +native %[virtuals=c] gcc", ["%[virtuals=c deptypes=build,link] gcc"], []),
+        # Package that depends on llvm as a library and also needs C and C++ compilers
+        (
+            "llvm-client",
+            ["%[virtuals=c,cxx deptypes=build] gcc", "%[deptypes=build,link] llvm"],
+            ["%c=llvm"],
+        ),
+        (
+            "llvm-client %c,cxx=gcc",
+            ["%[virtuals=c,cxx deptypes=build] gcc", "%[deptypes=build,link] llvm"],
+            ["%c=llvm"],
+        ),
+        ("llvm-client %c,cxx=llvm", ["%[virtuals=c,cxx deptypes=build,link] llvm"], ["%gcc"]),
     ],
 )
 def test_specifying_direct_dependencies(