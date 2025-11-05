e0d322ddb4714cb9442c68390b24123abcda0754
Peter Scheibel <scheibel1@llnl.gov>
2025-11-05 15:57:43 -0800

    concretization: improve error messages for requirements (#45800)
    This PR adds a number of tests that exercise error messages and
    adjusts weights on error messages in the solver to improve the quality
    of messages of the tests.
    
    The tests added are all designed to create a concretization attempt that will fail
    because the problem is over-constrained. Some tests add constraints to package
    definitions; some add requirements to configuration. The tests verify that a good
    error message is achieved for each test case.
    
    Specific improvements:
    
    * The concretizer has the freedom to choose a wrong version. The error weights
      for this type of message was low, so it could short-circuit a cause chain by just
      assigning a wrong version. The version error mechanism is now split in two: 
    
      1. Arbitrary version selections that lead to version errors are penalized heavily.
      2. Version errors that result from other constraints are not.
    
    * When this PR was first created, the error messages for requirements were
      particularly bad. They have been broadly improved by heavily encouraging
      requirement groups to apply at least one of their constraints, which appears
      to properly hook into the causal chaining logic in `error_messages.lp`.
    
    ---
    
    Signed-off-by: Peter Scheibel <scheibel1@llnl.gov>

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -2196,11 +2196,16 @@ class SpackSolverSetup:
                     # else: for virtuals we want to emit "node" and
                     # "virtual_node" in imposed specs
 
+                    info_msg = f"{input_spec} is a requirement for package {pkg_name}"
+                    if rule.condition != spack.spec.Spec():
+                        info_msg += f" when {rule.condition}"
+                    if rule.message:
+                        info_msg += f" ({rule.message})"
                     member_id = self.condition(
                         required_spec=when_spec,
                         imposed_spec=spec,
                         required_name=pkg_name,
-                        msg=f"{input_spec} is a requirement for package {pkg_name}",
+                        msg=info_msg,
                         context=context,
                     )
 
