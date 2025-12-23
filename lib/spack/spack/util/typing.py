a31315fe45da889ec2bb5e31de68a2cdcc599647
Harmen Stoppels <me@harmenstoppels.nl>
2025-12-23 22:00:20 +0100

    `util/typing.py`: eliminate runtime overhead (#51781)
    Inheriting from `Protocol` can be very expensive at runtime if subclasses
    make use of `isinstance()`.  
    
    In Spack, this comes from `typing._ProtocolMeta.__instancecheck__` being
    called hundreds of thousands of times, mostly thanks to version (list) satisfies
    related function calls, which do various isinstance checks. That function is
    implemented in Python, whereas normal isinstance checks dispatch to a C function.
    
    The `SupportsRichComparison` protocol is only used during type checking, and
    we do not need it at runtime at all. So, we can just replace it with `object` when it's
    not used for type checking.  This should allow us to eventually get rid of
    `# type: ignore` comments while not sacrificing any runtime performance.
    
    Simply adding:
    
    ```python
    if not TYPE_CHECKING:
        SupportsRichComparison = object
    ```
    
    Results in:
    
    ```
    hyperfine --runs 3 'spack spec --fresh sirius'
    ```
    
    a 2.5% reduction in runtime, and
    
    ```
    python3 -c 'import spack.repo; x = list(spack.repo.PATH.all_package_classes())'
    ```
    
    seems to have a 4.8% runtime reduction.
    
    ---------
    
    Signed-off-by: Harmen Stoppels <me@harmenstoppels.nl>
    Signed-off-by: Todd Gamblin <tgamblin@llnl.gov>
    Co-authored-by: Todd Gamblin <tgamblin@llnl.gov>

--- lib/spack/spack/util/typing.py
+++ lib/spack/spack/util/typing.py
@@ -1,8 +1,18 @@
 # Copyright Spack Project Developers. See COPYRIGHT file for details.: object
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
+"""Extra support for type checking in Spack.
 
-from typing import Any
+Protocols here that have runtime overhead should be set to ``object`` when
+``TYPE_CHECKING`` is not enabled, as they can incur unreasonable runtime overheads.
+
+In particular, Protocols intended for use on objects that have many ``isinstance()``
+calls can be very expensive.
+
+"""
+
+
+from typing import TYPE_CHECKING, Any
 
 from spack.vendor.typing_extensions import Protocol
 
@@ -27,3 +37,7 @@ class SupportsRichComparison(Protocol):
 
     def __ge__(self, other: Any) -> bool:
         raise NotImplementedError
+
+
+if not TYPE_CHECKING:
+    SupportRichComparison = object
